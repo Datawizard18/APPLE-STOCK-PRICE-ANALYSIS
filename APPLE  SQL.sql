83ba0bbb310b22498069738a7464cb6b9957ceab
Datawizard18 <sindhuja080821@gmail.com>
2024-11-18 20:43:05 +0530

    Add files via upload

---
+++ APPLE  SQL.sql
@@ -0,0 +1,10 @@
+USE SINDHU;
+CREATE TABLE APPLE (
+date_apple DATE,
+Open_apple int,
+high int,
+lowi int,
+close_apple int,
+adjustment_close int,
+volume int);
+select * from apple_after_preprocessing;
